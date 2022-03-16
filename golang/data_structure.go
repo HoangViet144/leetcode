ec02c057b949e7619580e83934e26b5134c90132
Trần Hoàng Việt <54036989+HoangViet144@users.noreply.github.com>
2022-03-16 09:01:40 +0700

    .

--- golang/data_structure.go
+++ golang/data_structure.go
@@ -1,1 +1,17 @@
+type IntStack struct {
+    ar []int
+}
 
+func (st *IntStack) push (ele int){
+    st.ar = append(st.ar, ele)
+}
+
+func (st *IntStack) size () int {
+    return len(st.ar)
+}
+
+func (st *IntStack) pop() int {
+    topEle := st.ar[st.size()-1]
+    st.ar = st.ar[:st.size()-1]
+    return topEle
+}
