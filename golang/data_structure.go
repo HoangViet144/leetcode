02f7357763363df9c52f02a33fdd6b0a8fbe046e
Trần Hoàng Việt <54036989+HoangViet144@users.noreply.github.com>
2022-03-16 09:59:15 +0700

    .

--- golang/data_structure.go
+++ golang/data_structure.go
@@ -35,3 +35,66 @@ func (st *Stack) pop() interface{} {
 	return headNode.value
 }
 
+func (st *Stack) push(value interface{}) {
+	st.head = &node{value,st.head}
+	st.length++
+}
+
+\\-------------------------------------------------\\
+
+type node struct {
+    value interface{}
+    next *node
+}	
+
+type Queue struct {
+    head *node
+    tail *node
+    length int
+}
+
+func NewQueue() *Queue {
+	return &Queue{nil,nil,0}
+}
+
+func (q *Queue) size() int {
+	return q.length
+}
+
+func (q *Queue) top() interface{} {
+	if q.length == 0 {
+		return nil
+	}
+	return q.head.value
+}
+
+func (q *Queue) deque() interface{} {
+	if q.length == 0 {
+		return nil
+	}
+	
+	headNode := q.head
+	q.head = headNode.next
+	q.length--
+    if q.length == 0 {
+        q.tail = nil
+    }
+	return headNode.value
+}
+
+func (q *Queue) enque(value interface{}) {
+    newNode := &node{value, nil}
+    if q.head == nil {
+        q.head = newNode
+        q.tail = newNode
+    } else {
+        q.tail.next = newNode
+        q.tail = q.tail.next
+    }
+	q.length++
+}
+
+func (q *Queue) empty() bool {
+    return q.size() == 0
+}
+
