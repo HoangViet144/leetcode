1ada3dc90baa5e6d4515de535fda3207014f83bc
Trần Hoàng Việt <54036989+HoangViet144@users.noreply.github.com>
2022-03-16 09:18:24 +0700

    .

--- golang/data_structure.go
+++ golang/data_structure.go
@@ -11,7 +11,17 @@ func (st *IntStack) size () int {
 }
 
 func (st *IntStack) pop() int {
+    if st.size() == 0 {
+        return int(1<<31)
+    }
     topEle := st.ar[st.size()-1]
     st.ar = st.ar[:st.size()-1]
     return topEle
+}
+
+func (st *IntStack) top() int {
+    if st.size() == 0 {
+        return int(1<<31)
+    }
+    return st.ar[st.size()-1]
 }
